a844a141cc73b8c453c1146d5b2769512270428b
Andrii Kovalskyi <andrii.kovalskyi02@gmail.com>
2023-12-22 21:02:46 +0100

    Project created

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <PokeMove />
+  </main>
+</template>
+
+<script setup lang="ts">
+import PokeMove from './components/PokeMove.vue'
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/PokeMove.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="move">
+      
+  </div>
+</template>
+
+<script setup lang="ts">
+// defineProps<{
+//   msg: string
+// }>()
+</script>
+
+<style scoped lang="scss">
+
+</style>
